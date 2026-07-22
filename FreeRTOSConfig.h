8f23829231c69cc8448339c5064a9a3963ce41d9
calebe-sucupira <calebesucupira.o@gmail.com>
2026-07-21 23:30:52 -0300

    Configura build modular com FreeRTOS

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -77,8 +77,8 @@
 #define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
-#define configCHECK_FOR_STACK_OVERFLOW          0
-#define configUSE_MALLOC_FAILED_HOOK            0
+#define configCHECK_FOR_STACK_OVERFLOW          2
+#define configUSE_MALLOC_FAILED_HOOK            1
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
 
 /* Run time and task stats gathering related definitions. */
@@ -103,11 +103,10 @@
 #define configMAX_API_CALL_INTERRUPT_PRIORITY   [dependent on processor and application]
 */
 
-/* SMP port only */
-#define configNUM_CORES                         2
+/* RP2040 single-core configuration */
+#define configNUMBER_OF_CORES                   1
 #define configTICK_CORE                         0
-#define configRUN_MULTIPLE_PRIORITIES           1
-#define configUSE_CORE_AFFINITY                 1
+#define configUSE_CORE_AFFINITY                 0
 
 /* RP2040 specific */
 #define configSUPPORT_PICO_SYNC_INTEROP         1
